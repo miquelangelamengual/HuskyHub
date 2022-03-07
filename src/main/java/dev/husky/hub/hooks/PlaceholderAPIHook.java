827ba48bc27eedae927c44c71a78daaa36172a53
Miquel Angel <hulkrojo2@gmail.com>
2022-03-07 07:14:27 +0100

    added classes for hooks

---
+++ src/main/java/dev/husky/hub/hooks/PlaceholderAPIHook.java
@@ -0,0 +1,11 @@
+package dev.husky.hub.hooks;
+
+/**
+ * Created by Hulk
+ * at 07/03/2022 7:14
+ * Project: HuskyHub
+ * Class: PlaceholderAPIHook
+ */
+
+public class PlaceholderAPIHook {
+}
---
+++ src/main/java/dev/husky/hub/hooks/TablistHook.java
@@ -0,0 +1,11 @@
+package dev.husky.hub.hooks;
+
+/**
+ * Created by Hulk
+ * at 07/03/2022 7:13
+ * Project: HuskyHub
+ * Class: TablistHook
+ */
+
+public class TablistHook {
+}
