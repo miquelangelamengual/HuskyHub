24134a229b4667475074bbf0891ff1d27a9a9cc9
Miquel Angel <hulkrojo2@gmail.com>
2022-03-07 10:38:40 +0100

    added command api

---
+++ src/main/java/dev/husky/hub/utils/command/BaseCommand.java
@@ -0,0 +1,10 @@
+package dev.husky.hub.utils.command;
+
+public abstract class BaseCommand {
+
+    public BaseCommand() {
+        CommandManager.getInstance().registerCommands(this, null);
+    }
+
+    public abstract void onCommand(CommandArgs command);
+}
