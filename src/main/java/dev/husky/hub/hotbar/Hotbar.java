900c1b67b5f07bec4bef2a10428a7584aeeddb55
Miquel Angel <hulkrojo2@gmail.com>
2022-03-07 11:47:42 +0100

    finished hotbar items

--- src/main/java/dev/husky/hub/hotbar/Hotbar.java
+++ src/main/java/dev/husky/hub/hotbar/Hotbar.java
@@ -1,5 +1,7 @@
 package dev.husky.hub.hotbar;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -10,6 +12,8 @@ import org.bukkit.inventory.ItemStack;
  * Class: Hotbar
  */
 
+@Getter
+@Setter
 public class Hotbar {
 
     private ItemStack enderbutt;
@@ -19,18 +23,23 @@ public class Hotbar {
     private ItemStack show;
 
     public void init() {
-        if (HotbarItem.isEnabled("ENDERBUTT")) {
-            enderbutt = HotbarItem.createItem("ENDERBUTT");
-        }
-
-        if (HotbarItem.isEnabled("SERVER_SELECTOR")) {
-            serverSelector = HotbarItem.createItem("SERVER_SELECTOR");
-        }
+        if (HotbarItem.isEnabled("ENDERBUTT")) enderbutt = HotbarItem.createItem("ENDERBUTT");
+        if (HotbarItem.isEnabled("SERVER_SELECTOR")) serverSelector = HotbarItem.createItem("SERVER_SELECTOR");
+        if (HotbarItem.isEnabled("LOBBY_SELECTOR")) lobbySelector = HotbarItem.createItem("LOBBY_SELECTOR");
+        if (HotbarItem.isEnabled("PLAYER_VISIBILITY_OFF")) hide = HotbarItem.createItem("PLAYER_VISIBILITY_OFF");
+        if (HotbarItem.isEnabled("PLAYER_VISIBILITY_ON")) show = HotbarItem.createItem("PLAYER_VISIBILITY_ON");
     }
 
     public void givePlayerItems(Player player) {
-        if (HotbarItem.isEnabled("ENDERBUTT")) {
-            player.getInventory().setItem(HotbarItem.getSlot("ENDERBUTT") -1, enderbutt);
-        }
+        if (HotbarItem.isEnabled("ENDERBUTT"))
+            player.getInventory().setItem(HotbarItem.getSlot("ENDERBUTT") - 1, enderbutt);
+        if (HotbarItem.isEnabled("SERVER_SELECTOR"))
+            player.getInventory().setItem(HotbarItem.getSlot("SERVER_SELECTOR") - 1, serverSelector);
+        if (HotbarItem.isEnabled("LOBBY_SELECTOR"))
+            player.getInventory().setItem(HotbarItem.getSlot("LOBBY_SELECTOR") - 1, lobbySelector);
+        if (HotbarItem.isEnabled("PLAYER_VISIBILITY_OFF"))
+            player.getInventory().setItem(HotbarItem.getSlot("PLAYER_VISIBILITY_OFF") - 1, hide);
+        if (HotbarItem.isEnabled("PLAYER_VISIBILITY_ON"))
+            player.getInventory().setItem(HotbarItem.getSlot("PLAYER_VISIBILITY_OFF") - 1, show);
     }
 }
